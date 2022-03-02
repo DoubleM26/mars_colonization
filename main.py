4e251cface0f260ca93abe75fcd3238f20c22bc0
doubleM26 <misha.261206@yandex.ru>
2022-03-02 23:37:24 +0300

    Варианты выбора

--- main.py
+++ main.py
@@ -13,6 +13,36 @@ def main():
     return "Миссия Колонизация Марса"
 
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name}</h1>
+                        <div class="alert alert-primary" role="alert">
+                          На ней много необходимых ресурсов
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                          На ней есть вода и атмосфера
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                          На ней есть небольшое магнитное поле
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                          Она красива!
+                        </div>
+                      </body>
+                    </html>'''
+
+
 @app.route('/promotion')
 def promotion():
     promos = ["Человечество вырастает из детства.",
