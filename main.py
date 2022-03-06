4827ae3db1c47d7ac569ef6097493addbfe4e5f3
doubleM26 <misha.261206@yandex.ru>
2022-03-06 22:20:07 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -8,5 +8,20 @@ def index(title):
     return render_template("base.html", title=title)
 
 
+@app.route("/training/<prof>")
+def training(prof):
+    if "инженер" in prof:
+        img = "..\static\img\инженер.png"
+    elif "строитель" in prof:
+        img = "..\static\img\строитель.png"
+    else:
+        img = "..\static\img\другие.png"
+    if prof in ["врач", "химик", "физик", "астронавт"]:
+        text = "Научные симуляторы"
+    else:
+        text = "Инженерные тренажеры"
+    return render_template("training.html", img=img, text=text)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
