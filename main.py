d1ef1649ff4603207750ef4aa436b84b6ad8b6b0
doubleM26 <misha.261206@yandex.ru>
2022-03-06 21:13:13 +0300

    Пейзажи Марса

--- main.py
+++ main.py
@@ -13,6 +13,47 @@ def main():
     return "Миссия Колонизация Марса"
 
 
+@app.route("/landscapes")
+def landscapes():
+    return f"""
+    <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossorigin="anonymous">
+</head>
+<body>
+<h1>Пейзажи Марса</h1>
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="{url_for('static', filename='img/landscape_1.jpg')}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="{url_for('static', filename='img/landscape_2.jpg')}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{url_for('static', filename='img/landscape_3.jpg')}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</body>
+</html>
+    """
+
+
 @app.route('/choice/<planet_name>')
 def choice(planet_name):
     if planet_name == "марс":
