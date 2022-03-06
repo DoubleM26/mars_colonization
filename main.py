b4969fb17794c3d8f31601ff4546e65cbdb2e681
doubleM26 <misha.261206@yandex.ru>
2022-03-06 21:55:10 +0300

    Готовимся к миссии

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/index/<title>")
+def index(title):
+    return render_template("base.html", title=title)
 
 
 if __name__ == '__main__':
