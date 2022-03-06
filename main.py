5a863ccc0fa61c27f2b0dad6bc4eb61ac16c2900
doubleM26 <misha.261206@yandex.ru>
2022-03-06 22:42:10 +0300

    Список профессий

--- main.py
+++ main.py
@@ -23,5 +23,10 @@ def training(prof):
     return render_template("training.html", img=img, text=text)
 
 
+@app.route("/list_prof/<li>")
+def list_prof(li):
+    return render_template("list_prof.html", li=li)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
