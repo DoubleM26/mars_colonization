fb29677a8758dc384f54529f2bc7453743c18711
doubleM26 <misha.261206@yandex.ru>
2022-03-08 13:57:42 +0300

    Цвет каюты

--- main.py
+++ main.py
@@ -60,5 +60,23 @@ def distribution():
     return render_template('distribution.html', astronauts=astronauts, str=str)
 
 
+@app.route("/table/<sex>/<age>")
+def table(sex, age):
+    age = int(age)
+    if age < 21:
+        img = "../../static/img/kid.jpg"
+    else:
+        img = "../../static/img/adult.jpg"
+
+    color = round(age * 255 / 100)
+    color = 255 - color
+    if sex == "female":
+        color = f"rgb(255, {color}, {color})"
+    else:
+        color = f"rgb({color}, {color}, 255)"
+
+    return render_template('table.html', img=img, color=color)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
