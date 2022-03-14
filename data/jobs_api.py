1467e331b4e6263096d6c818fd681a3736396005
doubleM26 <misha.261206@yandex.ru>
2022-03-14 17:42:31 +0300

    Добавление работы

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -33,3 +33,25 @@ def get_job(job_id):
             'job': jobs.to_dict()
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job', 'work_size', 'collaborators', 'start_date', "end_date", "is_finished"]):
+        return jsonify({'error': 'Bad request'})
+    db_sess = db_session.create_session()
+    jobs = Jobs(
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        start_date=request.json['start_date'],
+        end_date=request.json['end_date'],
+        is_finished=request.json['is_finished']
+    )
+    db_sess.add(jobs)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
